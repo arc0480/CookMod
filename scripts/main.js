4e853b6c9fe4059b5ce761f5da5e6c022e06184a
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 08:40:11 +0900

    main.js を更新

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 //jsで記載
+
+//ダイアログを表示
+const myDialog = new BaseDialog("Dialog Title");
+// Add "go back" button
+myDialog.addCloseButton();
+// Add text to the main content
+myDialog.cont.add("Goodbye.");
+// Show dialog
+myDialog.show();
