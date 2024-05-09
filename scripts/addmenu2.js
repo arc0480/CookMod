6b986182beb50e9f867477d251639d9ac89bf662
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 23:07:56 +0900

    Update addmenu2.js

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,5 +1,15 @@
 Events.on(ClientLoadEvent, () => {
-    Vars.ui.hudfrag.addButton("Hi", Icon.trash,  () => {
-    
-    });
+    const myStyle = Object.assign(new TextButtonStyle(), {
+  over: Tex.buttonRightOver,
+  down: Tex.buttonRightDown,
+  up: Tex.buttonRight,
+  disabled: Tex.buttonRightDisabled,
+  font: Fonts.def,
+  fontColor: Color.white,
+  disabledFontColor: Color.gray,
+});
+
+wavesTable.button("Hi", myStyle, () => {
+  // code
+}).grow();
 });
