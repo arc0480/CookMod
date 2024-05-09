21a2ccd1e723bf5c96a9bb8f339a333d05391091
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 12:24:18 +0900

    main.js を更新

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,7 @@ Events.on(ClientLoadEvent, () => {
 
 Vars.ui.menufrag.addButton("buttonNameHere", Icon.trash), () =>  {
 /* code */
-    Vars.ui.showConfirm"[red]Are yousure you want to reset your UUID?InThis action CANNOT be undone.",（) => ｛
     
-    "Choose an option",  // ダイアログのメッセージ
-    "Option 1",         // ボタン1のテキスト
-    () => {
-        // ボタン1がクリックされた時のアクション
-        Vars.ui.showInfo("Option 1 selected.");
-    },
-    "Option 2",         // ボタン2のテキスト
-    () => {
-        // ボタン2がクリックされた時のアクション
-        Vars.ui.showInfo("Option 2 selected.");
-    }
-    }
 });
 
 });
