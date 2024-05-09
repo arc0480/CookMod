c0116b2eea834b3dc0c5f6d50b9562fc3666da60
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 12:09:42 +0900

    main.js を更新

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,7 @@
-// JavaScriptでのメニュー作成例
-Vars.ui.showCustomConfirm(
-    "Choose an option",  // ダイアログのメッセージ
-    "Option 1",         // ボタン1のテキスト
-    () => {
-        // ボタン1がクリックされた時のアクション
-        Vars.ui.showInfo("Option 1 selected.");
-    },
-    "Option 2",         // ボタン2のテキスト
-    () => {
-        // ボタン2がクリックされた時のアクション
-        Vars.ui.showInfo("Option 2 selected.");
-    }
-);
+Events.on(ClientLoadEvent, () => {});
+
+
+Vars.ui.menufrag.addButton("buttonNameHere", Core.atlas.find("buttonIconHere"), () => 
+    {/* code */
+        
+    });
