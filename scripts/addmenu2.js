24115232b4d901b1881e984f74061fe5c6f43adb
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 19:10:52 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -20,16 +20,17 @@
 //}) ;
 
 
-const myStyle = Object.assign (new TextButtonStyle(), 
-fover: Tex. buttonRightOver, 
-down: Tex.buttonRightDown, 
-up: Tex. buttonRight, 
-disabled:Tex. buttonRightDisabled, 
-font: Fonts.def, 
-fontColor: Color.white, 
-disabledFontColor: Color gray,
-}) ;
-wavesTable.button ("Hi",myStyle,() => {
+const myStyle = Object.assign (new TextButtonStyle(), {
+    over: Tex. buttonRightOver, 
+    down: Tex.buttonRightDown, 
+    up: Tex. buttonRight, 
+    disabled:Tex. buttonRightDisabled, 
+    font: Fonts.def, 
+    fontColor: Color.white, 
+    disabledFontColor: Color gray,
+});
+
+wavesTable.button("Hi",myStyle,() => {
 // code
 }).grow();
 
