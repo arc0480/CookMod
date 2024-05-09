b17578bdc1eb8f42c1fe4cc77432e6709db0c2e6
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 23:16:10 +0900

    Update addmenu2.js

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,17 +1,44 @@
+
+Events.on(ClientLoadEvent, e => {
+
+
+  
+const wavesTable = Vars.ui.hudGroup.find("waves");
+
+const [ statustable, infoTable ] = wavesTable.getChildren().items;
+const [ statustableCell, infoTableCell ] = wavesTable.getCells().items;
+  
   const myStyle = Object.assign(new TextButtonStyle(), {
-  over: Tex.buttonRightOver,
-  down: Tex.buttonRightDown,
-  up: Tex.buttonRight,
-  disabled: Tex.buttonRightDisabled,
-  font: Fonts.def,
-  fontColor: Color.white,
-  disabledFontColor: Color.gray,
+      over: Tex.buttonRightOver,
+      down: Tex.buttonRightDown,
+      up: Tex.buttonRight,
+      disabled: Tex.buttonRightDisabled,
+      font: Fonts.def,
+      fontColor: Color.white,
+      disabledFontColor: Color.gray,
+  });
+
+  wavesTable.clearChildren();
+    
+    wavesTable.add(statustable).set(statustableCell).growX();
+    
+    wavesTable.row();
+    
+    wavesTable.table(Tex.buttonRight, t => {
+        t.add("My custom table!");
+    }).growX();
+    
+    wavesTable.row();
+    
+    wavesTable.add(infoTable).set(infoTableCell).growX();
 });
 
+  
 
 
-Events.on(ClientLoadEvent, () => {
-    const wavesTable = Vars.ui.hudGroup.find("waves");
+
+
+    
     wavesTable.button("Hi", myStyle, () => {
   // code
     }).grow();
