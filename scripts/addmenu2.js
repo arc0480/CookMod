e75e7fc18d3dda27ea59dd8fc8190c9c1ce73ae2
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 20:15:31 +0900

    Update addmenu2.js

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,33 +1,11 @@
-//const { ImageButtonStyle } = ImageButton;
-
-Events.on(ClientLoadEvent, e => {
-    const wavesTable = Vars.ui.hudGroup.find("waves");
-    
-    const [ statustable, infoTable ] = wavesTable.getChildren().items;
-    const [ statustableCell, infoTableCell ] = wavesTable.getCells().items;
-    
-    // To remove the edge style. Change the style of skip button.
-    const style = Object.assign(new ImageButtonStyle(Styles.squarei), {
-        down: Styles.flatDown,
-        disabled: Tex.pane2,
-        imageUp: Icon.play,
-        imageDisabledColor: Color.clear,
-        imageUpColor: Color.white,
-    });
-    const skipButton = statustable.find("skip");
-   skipButton.setStyle(style);
-    
-    wavesTable.clearChildren();
-    
-    wavesTable.add(statustable).set(statustableCell).growX();
+const wavesTable = Vars.ui.hudGroup.find("waves");
+wavesTable.clearChildren();    
+wavesTable.add(statustable).set(statustableCell).growX();
     
     wavesTable.row();
     
     wavesTable.table(Tex.buttonRight, t => {
         t.add("My custom table!");
     }).growX();
-    
-    wavesTable.row();
-    
-    wavesTable.add(infoTable).set(infoTableCell).growX();
-});
+ wavesTable.row();
+wavesTable.add(infoTable).set(infoTableCell).growX();
