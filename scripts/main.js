044fa17fdd72c30d782a9a2945f53824961e192a
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 12:44:26 +0900

    main.js を更新

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 
 Events.on(ClientLoadEvent, () => {
+    //log
+    Log.error("err");
+    Log.warn("warn");
+    
+
 
     Vars.ui.menufrag.addButton("buttonNameHere", Icon.trash, () =>  {
     /* code */
@@ -8,3 +13,25 @@ Events.on(ClientLoadEvent, () => {
     );
 
 });
+
+
+Events.on(ClientLoadEvent, () => {
+    Log.error("Note: This UUID resetter is for testing & debugging purposes ONLY.");
+    Log.warn("I frankly don't want this mod to end up on the blacklist, so play fairly and respect your server bans.");
+    Vars.ui.menufrag.addButton("ResetUUID", Icon.trash, () => {
+        Vars.ui.showConfirm("[red]Are you sure you want to reset your UUID?\nThis action CANNOT be undone.", () => {
+            Core.settings.put("uuid","") 
+            Vars.platform.getUUID()
+            Call.infoMessage("Your UUID has been reset.")
+            Log.info("UUID was reset.")
+        });
+    });
+    Vars.ui.menufrag.addButton("Set UUID", Icon.save, () => {
+        Vars.ui.showTextInput("[red]If you mess this up and can't join servers, you must reset your UUID, as this tool will NOT check the new UUID for validity.\n[white]For your convenience, your current UUID has been inserted into the textbox below.","UUID:",Vars.platform.getUUID(), res => { 
+            Core.settings.put("uuid",res);
+            Call.infoMessage("Your UUID has been changed.")
+            Log.info("UUID was changed.")
+        });
+    });
+});
+```
