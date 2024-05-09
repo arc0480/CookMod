7e8ccdf50422f0490df7f3811cf2f38110fcddde
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 21:46:54 +0900

    Update addmenu2.js

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,11 +1,13 @@
-const wavesTable = Vars.ui.hudGroup.find("waves");
-wavesTable.clearChildren();    
-wavesTable.add(statustable).set(statustableCell).growX();
-    
-    wavesTable.row();
-    
-    wavesTable.table(Tex.buttonRight, t => {
-        t.add("My custom table!");
-    }).growX();
- wavesTable.row();
-wavesTable.add(infoTable).set(infoTableCell).growX();
+const myStyle = Object.assign(new TextButtonStyle(), {
+  over: Tex.buttonRightOver,
+  down: Tex.buttonRightDown,
+  up: Tex.buttonRight,
+  disabled: Tex.buttonRightDisabled,
+  font: Fonts.def,
+  fontColor: Color.white,
+  disabledFontColor: Color.gray,
+});
+
+wavesTable.button("Hi", myStyle, () => {
+  // code
+}).grow();
