f53b152b396e80c4de7357c76cec2f63cb8703e7
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 19:41:33 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,10 +1,14 @@
 
-  wavesTable.table(Tex.buttonRight, t => {
-  t.button("Hi", () => {
-    let myDialog = new BaseDialog("Title");
-    myDialog.addCloseButton();
-    myDialog.show();
-  }).growX().fillX().name("waveButton");
-  const myButton = t.find("waveButton");
-  myButton.setStyle(Styles.cleart);
-}).growX().fillX();
+const myStyle = Object.assign(new TextButtonStyle(), {
+  over: Tex.buttonRightOver,
+  down: Tex.buttonRightDown,
+  up: Tex.buttonRight,
+  disabled: Tex.buttonRightDisabled,
+  font: Fonts.def,
+  fontColor: Color.white,
+  disabledFontColor: Color.gray,
+});
+
+wavesTable.button("Hi", myStyle, () => {
+  // code
+}).grow();
