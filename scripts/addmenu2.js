99ec95766edbc616faa00976167715c07b1ddc0c
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 13:02:26 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,1 +1,15 @@
 
+// JavaScriptでのメニュー作成例
+Vars.ui.showCustomConfirm(
+    "Choose an option",  // ダイアログのメッセージ
+    "Option 1",         // ボタン1のテキスト
+    () => {
+        // ボタン1がクリックされた時のアクション
+        Vars.ui.showInfo("Option 1 selected.");
+    },
+    "Option 2",         // ボタン2のテキスト
+    () => {
+        // ボタン2がクリックされた時のアクション
+        Vars.ui.showInfo("Option 2 selected.");
+    }
+);
