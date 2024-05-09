1cee7f26c95cdafbf9d1b0b300c47851d21db0ec
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 13:35:11 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,5 +1,7 @@
 Events.on(ClientLoadEvent, () => {
-// JavaScriptでのメニュー作成例
-Vars.ui.showCustomConfirm.addButton("buttonNameHere", Icon.trash, () =>  {
-};
-};
+    // JavaScriptでのメニュー作成例
+    Vars.ui.showCustomConfirm.addButton("buttonNameHere", Icon.trash, () =>  {
+    }
+    );
+
+});
