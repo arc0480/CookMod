ee5c4954a54cb54c8ede998a836ba3d629101cbc
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 15:25:46 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,10 +1,17 @@
+Events.on(ClientLoadEvent, () => {
+    //log
+    //Log.error("err");
+    //Log.warn("warn");
+
+    
 
 
-    Vars.ui.consolefrag.addButton("buttonNameHere", Icon.trash, () =>  {
+    Vars.ui.menufrag.addButton("buttonNameHere", Icon.trash, () =>  {
     /* code */
     
-    }
-    );
+    });
+});
+
 
 
 
