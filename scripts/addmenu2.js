3f440d2668b6e00935ce41f8c8588511e4d418d0
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 22:43:41 +0900

    Update addmenu2.js

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -1,5 +1,6 @@
 const { ImageButtonStyle } = ImageButton;
 Events.on(WorldLoadEndEvent, e => {
+    const wavesTable = Vars.ui.hudGroup.find("waves");
     wavesTable.table(Tex.buttonRight, t => {
     t.button("Hi", Styles.cleart, () => {
         // onclick
