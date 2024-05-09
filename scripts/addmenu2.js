d846e5eb6b83bd63cb5a7bca45662122e078d25e
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 19:02:23 +0900

    addmenu2.js を更新

--- scripts/addmenu2.js
+++ scripts/addmenu2.js
@@ -13,11 +13,24 @@
 //    });
 //});
 
-Attribute. add ("attribute");
-Events.on(ContentInitEvent, e => {
-Vars.content.block("block").attrib
-utes.set（Attribute.get（"attributes"), 1) ;
+//Attribute. add ("attribute");
+//Events.on(ContentInitEvent, e => {
+//Vars.content.block("block").attrib
+//utes.set（Attribute.get（"attributes"), 1) ;
+//}) ;
+
+
+const myStyle = Object.assign (new
+TextButtonStyle(), f
+over: Tex. buttonRightOver, down: Tex.buttonRightDown, up: Tex. buttonRight, disabled:
+Tex. buttonRightDisabled, font: Fonts.def, fontColor: Color. white, disabledFontColor: Color gray,
 }) ;
+wavesTable.button ("Hi"
+, myStyle,
+() => {
+// code
+}) • grow();
+Im actually not sure if 
 
 
 
