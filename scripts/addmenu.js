cf6d6bb5f93283ef9c8da007b95ac4824cd3be8f
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 19:31:41 +0900

    addmenu.js を更新

--- scripts/addmenu.js
+++ scripts/addmenu.js
@@ -12,6 +12,19 @@ Events.on(ClientLoadEvent, () => {
     /* ボタンタップ時の処理を記載？ */
     
     });
+    
+    wavesTable.table(Tex.buttonRight, t => {
+  t.button("Hi", () => {
+    let myDialog = new BaseDialog("Title");
+    myDialog.addCloseButton();
+    myDialog.show();
+  }).growX().fillX().name("waveButton");
+  const myButton = t.find("waveButton");
+  myButton.setStyle(Styles.cleart);
+}).growX().fillX();
+
+    
+    
 });
 
 
