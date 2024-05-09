869e03280c5214ea541333dda272cb853d1c32f8
arc <145819704+arc0480@users.noreply.github.com>
2024-05-09 10:05:10 +0900

    main.js を更新

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,22 @@ function sliderTable(table){
     
    table.visibility = () => !folded && visibility();
 }
+
+Events.on(EventType.ClientLoadEvent, () => {
+    const dialog = new BaseDialog("Info");
+    const creators = new BaseDialog("Creators");
+    creators.cont.add("Spriting: Exter").row();
+    creators.cont.add("Coding: Exter").row();
+    dialog.cont.add("This mod is WIP, so there can be bugs. ").row();
+    dialog.cont.button("OK ", () => {
+        dialog.hide();
+    }
+    dialog.cont.button("Creators ", () => {
+        creators.show();
+    }
+    creators.cont.button("OK ", () => {
+        creators.hide();
+    }
+    size(100, 50);
+    dialog.show();
+}`
